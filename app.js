858b1aca4975f7a44cb4ef40048085549f1148a9
DexMills <dex.m.mills@gmail.com>
2017-03-15 17:17:29 -0600

    feat (main) Add button listener to JS file

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-console.log("is this hooked up?")
-function onload(){
+function onLoad(){
+  console.log("is this hooked up?")
 
-  function add(num1, num2){
+  var buttons =
+   document.querySelectorAll('button');
+   for (let item of buttons){
+     item.addEventListener("click", function () {
+      console.log(item.value);
+    });
+   }
+   console.log(buttons)
+ }
+
+
+
+
+  function addNum(num1, num2){
     var sum = (num1+num2);
     console.log(sum);
     return sum;
   }
-  function sub(num1, num2){
+  function subNum(num1, num2){
     var sub =(num1-num2);
     console.log(sub);
     return sub;
   }
-  function mult(num1,num2){
+  function multNum(num1,num2){
     var mult =(num1*num2);
     console.log(mult);
     return mult;
   }
-  function divd(num1, num2){
+  function divdNum(num1, num2){
     if (num2 === 0){
       console.log('error')
     }
@@ -25,26 +38,49 @@ function onload(){
     console.log(divd);
     return divd;
   }
+
+
+function equalsAns(arr,cb){
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++){
+      if (cb(arr[i])) {
+        sum == arr[i];
+      }
+    }
+    return sum;
   }
 
-function equals(arr,cb){}
 
 
-
-
-//let display=
-let btn = document.getElementById("one");
-btn.addEventListener('click', function(){
-     console.log(btn.value);
-    });
-
-
-
-
-
-
-
-
+  const arr = [10, 5];
+  const addsAns= equalAns(arr, addNum);
+  console.log('Answer is', addsAns);
+  const subsAns= equalAns(arr, subNum);
+  console.log('Answer is', subsAns);
+  const multsAns= equalAns(arr, multNum);
+  console.log('Answer is', multAns);
+  const divdsAns= equalAns(arr,divdNum);
+  console.log('Answer is', divdsAns)
 
 }
-window.onload = onload;
+window.onload = onLoad;
+
+
+
+// const arr = [10, 5]
+// const adds= equalsAns(arr, add);
+// console.log(adds);
+// const subs= equalsAns(arr, sub);
+// console.log(subs);
+// const mults= equalsAns(arr, mult);
+// console.log(mult);
+// const divds= equalsAns(arr,divd);
+// console.log(divds)
+//
+// console.log(add(1,2))
+// console.log(boo())
+// function boo(){
+//   const x= 42;
+//   return x
+// }
