dedaa3a61758bffad8c73ca71774da3434091643
DexMills <dex.m.mills@gmail.com>
2017-03-15 22:15:02 -0600

    feat (main) Add display box and js for message in box

--- app.js
+++ app.js
@@ -1,86 +1,123 @@
 function onLoad(){
   console.log("is this hooked up?")
-
+  var display =[]
   var buttons =
    document.querySelectorAll('button');
    for (let item of buttons){
      item.addEventListener("click", function () {
-      console.log(item.value);
+     document.getElementById('calc-display').innerHTML=item.value;
+    display.push(item.value);
+    console.log(item.value);
+    document.getElementById('calc-display').innerHTML=display.join('');
+
     });
    }
-   console.log(buttons)
- }
+  //  console.log(buttons);
+  //  return buttons;
+  //
+  // function math(){
+  // if () {
+  //  var num1 =push.display;
+  //  else{
+  //    var num2 =push.display;
+  //  }
+
+
+      // console.log(num1);
+  //  // use this area to use button returns to display in input box. use getElementById="calc-dsplay" and insert value
+  //
+  //
+  // var array = array.push(button);
+  // console.log(buttons)
 
 
 
 
-  function addNum(num1, num2){
-    var sum = (num1+num2);
-    console.log(sum);
-    return sum;
-  }
-  function subNum(num1, num2){
-    var sub =(num1-num2);
-    console.log(sub);
-    return sub;
-  }
-  function multNum(num1,num2){
-    var mult =(num1*num2);
-    console.log(mult);
-    return mult;
-  }
-  function divdNum(num1, num2){
-    if (num2 === 0){
-      console.log('error')
-    }
-    else{
-    var divd=(num1/num2);
-    console.log(divd);
-    return divd;
-  }
-
-
-function equalsAns(arr,cb){
-    let sum = 0;
-
-    for(let i = 0; i < arr.length; i++){
-      if (cb(arr[i])) {
-        sum == arr[i];
-      }
-    }
-    return sum;
-  }
-
-
-
-  const arr = [10, 5];
-  const addsAns= equalAns(arr, addNum);
-  console.log('Answer is', addsAns);
-  const subsAns= equalAns(arr, subNum);
-  console.log('Answer is', subsAns);
-  const multsAns= equalAns(arr, multNum);
-  console.log('Answer is', multAns);
-  const divdsAns= equalAns(arr,divdNum);
-  console.log('Answer is', divdsAns)
-
+//   function addNum(num1, num2){
+//     var sum = (num1+num2);
+//     console.log(sum);
+//     return sum;
+//   }
+//   function subNum(num1, num2){
+//     var sub =(num1-num2);
+//     console.log(sub);
+//     return sub;
+//   }
+//   function multNum(num1,num2){
+//     var mult =(num1*num2);
+//     console.log(mult);
+//     return mult;
+//   }
+//   function divdNum(num1, num2){
+//     if (num2 === 0){
+//       console.log('error')
+//     }
+//     else{
+//     var divd=(num1/num2);
+//     console.log(divd);
+//     return divd;
+//   }}
+//
+// function equalAns(arr,cb){
+//     return (cb(arr[0], arr[1]))();
+//
+//    }
+// //
+// //
+// //
+// //
+//   const arr = [10, 5];
+//   const addsAns = equalAns(arr, addNum);
+//   console.log("Answer is", addsAns);
+//   const subsAns = equalAns(arr, subNum);
+//   console.log("Answer is", subsAns);
+//   const multsAns = equalAns(arr, multNum);
+//   console.log("Answer is", multsAns);
+//   const divdsAns = equalAns(arr,divdNum);
+//   console.log("Answer is", divdsAns);
 }
 window.onload = onLoad;
 
 
+function addNum(num1, num2){
+  var sum = (num1+num2);
+  console.log(sum);
+  return sum;
+}
+function subNum(num1, num2){
+  var sub =(num1-num2);
+  console.log(sub);
+  return sub;
+}
+function multNum(num1,num2){
+  var mult =(num1*num2);
+  console.log(mult);
+  return mult;
+}
+function divdNum(num1, num2){
+  if (num2 === 0){
+    console.log('error')
+  }
+  else{
+  var divd=(num1/num2);
+  console.log(divd);
+  return divd;
+}}
 
-// const arr = [10, 5]
-// const adds= equalsAns(arr, add);
-// console.log(adds);
-// const subs= equalsAns(arr, sub);
-// console.log(subs);
-// const mults= equalsAns(arr, mult);
-// console.log(mult);
-// const divds= equalsAns(arr,divd);
-// console.log(divds)
+function equalAns(arr,cb){
+  return (cb(arr[0], arr[1]))();
+
+ }
 //
-// console.log(add(1,2))
-// console.log(boo())
-// function boo(){
-//   const x= 42;
-//   return x
-// }
+//
+//
+//
+const arr = [10, 5];
+const addsAns = equalAns(arr, addNum);
+console.log("Answer is", addsAns);
+const subsAns = equalAns(arr, subNum);
+console.log("Answer is", subsAns);
+const multsAns = equalAns(arr, multNum);
+console.log("Answer is", multsAns);
+const divdsAns = equalAns(arr,divdNum);
+console.log("Answer is", divdsAns);
