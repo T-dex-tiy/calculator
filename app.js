d27ba322edb9d2befc198ce76f7d585a433bdded
T-Dex <dex.m.mills@gmail.com>
2017-04-26 16:20:49 -0600

    Add item to pull data from front end

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 function onLoad(){
   //This is running well. Refactor if needed/ever figure out a better way
   console.log("is this hooked up?")
+
+  var guiDisplay = []
   var display =[]
   var buttons =
    document.querySelectorAll('button');
    for (let item of buttons){
-     item.addEventListener("click", function () {
-     document.getElementById('calc-display').innerHTML=item.value;
-    display.push(item.value);
-    console.log(item.value);
-    document.getElementById('calc-display').innerHTML=display.join('');
+         item.addEventListener("click", function () {
+         document.getElementById('calc-display').innerHTML=item.value;
+         console.log(display);
+        display.push(item.value);
 
+        console.log(item.value);
+
+        document.getElementById('calc-display').innerHTML=display.join('');
+
+        display = [item.value];
+        guiDisplay.push(display);
+        console.log('display', display);
+        checkDisplay();
+
+    return display
     });
    }
-  //PLACING THIS FOR HOLDING UNTIL VAR DISTRUBUTION AFTER CLICK OF MATH FUNCTION
-  // function place(){
-  // if () {
-  //  var num1 =push.display;
-  //  else{
-  //    var num2 =push.display;
 
-  // var array = array.push(button);
-  // console.log(buttons)
+function updateDisplay(){
+
+  console.log('oustide:',display, guiDisplay);
+}
+// var num1=display[0];
+// var num2=display[2];
+// var mathFunc=display[1];
 
 
 
@@ -51,10 +61,37 @@ function onLoad(){
 //     return divd;
 //   }}
 //
-// function equalAns(arr,cb){
-//     return (cb(arr[0], arr[1]))();
+//  var mathFunc = {x} ;
+//  var num1 = 2;
+//  var num2 = 5;
+// function equalAns(){
+//     if (var mathFunc = 'x' );{
+//     function multNum(num1,num2);
+//     return (equalAns);
+//   }
+    // else if{
+    //   (var mathFunc = "+");{
+    //     function addNum(num1,num2);
+    //   }
+    // }
+    //   else if{
+    //     (var mathFunc = "-");{
+    //       function subNum(num1,num2);
+    //     }
+    //   }
+    //     else if {
+    //       (var mathFunc = '/');{
+    //         function divdNum(num1,num2);
+    //       }
+    //     }
+        // return equalAns;
+        // console.log(equalAns)
+      // }
 //
-//    }
+//
+// }
+//     //return (cb(arr[0], arr[1]))();
+//  }
 // //
 // //
 // //
@@ -70,48 +107,53 @@ function onLoad(){
 //   console.log("Answer is", divdsAns);
 }
 window.onload = onLoad;
+// var num1=display[0];
+// console.log(display[0]);
+// var num2=display[3];
 
-//Math checked out.
-function addNum(num1, num2){
-  var sum = (num1+num2);
-  console.log(sum);
-  return sum;
-}
-function subNum(num1, num2){
-  var sub =(num1-num2);
-  console.log(sub);
-  return sub;
-}
-function multNum(num1,num2){
-  var mult =(num1*num2);
-  console.log(mult);
-  return mult;
-}
-function divdNum(num1, num2){
-  if (num2 === 0){
-    console.log('error')
-  }
-  else{
-  var divd=(num1/num2);
-  console.log(divd);
-  return divd;
-}}
+// //Math checked out.
+// function addNum(num1, num2){
+//   var sum = (num1+num2);
+//   console.log(sum);
+//   return sum;
+// }
+// function subNum(num1, num2){
+//   var sub =(num1-num2);
+//   console.log(sub);
+//   return sub;
+// }
+// function multNum(num1,num2){
+//   var mult =(num1*num2);
+//   console.log(mult);
+//   return mult;
+// }
+// function divdNum(num1, num2){
+//   if (num2 === 0){
+//     console.log('error')
+//   }
+//   else{
+//   var divd=(num1/num2);
+//   console.log(divd);
+//   return divd;
+// }}
+// function equalAns(num){
+//     var display=[num1, function(cb),num2];
+//     console.log(num)
+//     return num;
 
-function equalAns(arr,cb){
-  return (cb(arr[0], arr[1]))();// This caused a nightmare in debugger and could not put together how to have call back run over multiple times.
+    //return (cb(arr[0], arr[1]))();
+// ]
+// function equals(){
+//   return ('var 1', function(cb), 'var 2');
+//
+// }
+//function equalAns(arr,cb){
+//return (cb(arr[0], arr[1]))();// This caused a nightmare in debugger and could not put together how to have call back run over multiple times.
 
- }
+ // }
 //
 //
 //
 //
-const arr = [10, 5];// Used for testing. Could not get to pass full arragement and left for testing purposes
-const addsAns = equalAns(arr, addNum);
-console.log("Answer is", addsAns);
-const subsAns = equalAns(arr, subNum);
-console.log("Answer is", subsAns);
-const multsAns = equalAns(arr, multNum);
-console.log("Answer is", multsAns);
-const divdsAns = equalAns(arr,divdNum);
-console.log("Answer is", divdsAns);
+//
 // Attempt to get my call backs working, would work for one only. Usually addition which would show on the console
