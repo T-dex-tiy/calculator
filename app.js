5da11a7abf963618422549a70fb565cc89f17787
T-Dex <dex.m.mills@gmail.com>
2018-09-01 12:36:00 -0600

    Feat(main) update css

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 
 function onLoad(){
   //This is running well. Refactor if needed/ever figure out a better way
-  console.log("is this hooked up?")
 
   var guiDisplay = []
   var maths = []
   var display=""
   var num1=[]
   var num2=[]
-  var mem=[]
+  var top=[]
   var digits =
    document.querySelectorAll('button.digit');
    for (let item of digits){
          item.addEventListener("click", function () {
          document.getElementById('calc-display').innerHTML=item.value;
          display += item.value;
+         let newDisplay=num1 +" "+ maths
+        document.getElementById('function').innerHTML=top
         document.getElementById('calc-display').innerHTML=display;
+        document.getElementById('function').innerHTML=newDisplay
         console.log('display', display);
     return display
     });
     
     
    }
-   
    var mathFunc =
     document.querySelectorAll('button.math');
     for (let item of mathFunc){
@@ -30,15 +31,21 @@ function onLoad(){
           document.getElementById('calc-display').innerHTML=item.value;
           if (num1.length==0){
             num1.push(display)
+            top.concat(num1)
+            console.log(top);
+            
             display=""
           }
-          console.log(num1, num2);
-          maths.push(item.value)
-          console.log(maths);
           
+          if(maths.length!=0){
+            return null
+          }else{
+          maths.push(item.value)}
+          console.log(maths);
          document.getElementById('calc-display').innerHTML=display;
-         
-         console.log('display', display);
+         let newDisplay= num1+' '+ maths
+         document.getElementById('function').innerHTML=newDisplay
+         console.log('display', newDisplay);
      return display
      });
     }
@@ -82,6 +89,8 @@ var answerDigits =
  for (let item of answerDigits){
        item.addEventListener("click", function () {
       num2.push(display)
+      let newDisplay=num1+" "+maths+' '+num2
+      document.getElementById('function').innerHTML=newDisplay;
       let number1 = parseFloat(num1[0]);
       let number2 = parseFloat(num2[0]);
       let mathFunc= maths[0].toString();
@@ -107,10 +116,14 @@ var answerDigits =
         display = runMathsFunc(number1, number2, whyNoWorky);
       }
       document.getElementById('calc-display').innerHTML=display;
+      
+     
+      
       num1=[]
       num2=[]
       maths=[]
       num1.push(display)
+
       display=""
       console.log(num1, display);
       
